e6fd3160f1bfc914ca5bf1f33e18fa37a1ff906c
Bruno Roberto Mueller <web@customax.com.br>
2022-11-22 19:04:07 -0300

    atualização

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ import TrackVisibility from "react-on-screen";
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Hi! I'm Bruno Mueller", "Full Stack Dev", "Coffee Lover <3"];
+  const toRotate = ["Hi! I'm Bruno!", "Full Stack Dev", "Coffee Lover <3"];
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
